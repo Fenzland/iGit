56e8fa6c18825f841f9f45da39e21d20ac894983
Fenz <uukoo@163.com>
2019-04-13 21:20:55 +0800

    a dragonfly Request construct with a deno Request

--- dragonfly/http/Request.js
+++ dragonfly/http/Request.js
@@ -1,5 +1,15 @@
 
 export default class Request
 {
-	
+	/**
+	 * Construct a HTTP request (server-side)
+	 * 
+	 * @param denoRequest ServerRequest
+	 */
+	constructor( denoRequest, )
+	{
+		this.url= denoRequest.url;
+		this.method= denoRequest.method;
+		this.headers= denoRequest.headers;
+	}
 }
