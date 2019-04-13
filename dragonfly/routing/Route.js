a25f662eb045de0731946a9286e62e8ff249621d
Fenz <uukoo@163.com>
2019-04-14 01:09:10 +0800

    Route constructs with path, method, accept and controller

--- dragonfly/routing/Route.js
+++ dragonfly/routing/Route.js
@@ -1,5 +1,22 @@
+import Response from '../http/Response.js';
 
 export default class Route
 {
-	
+	/**
+	 * Construct a route
+	 * 
+	 * @param 0.path       (string)       exact path pattern
+	 *                     {RegExp}       regexp path pattern, named groups for route params
+	 * @param 0.?method    (string)       HTTP method
+	 * @param 0.?accept    {AcceptArray}  HTTP accept
+	 * @param 0.controller (string)       controller module path [dynamic], [recommanded].
+	 *                     {Function}     built-in controller, generally for simple controllers.
+	 */
+	constructor( { path, method='*', accept='*/*', controller, }, )
+	{
+		this.path= path;
+		this.method= method;
+		this.accept= accept;
+		this.controller= controller;
+	}
 }
