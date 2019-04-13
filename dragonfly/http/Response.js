42ba74652cc347624bb2cd45a85a620de157f42b
Fenz <uukoo@163.com>
2019-04-13 23:06:21 +0800

    Response set what deno Response need

--- dragonfly/http/Response.js
+++ dragonfly/http/Response.js
@@ -1,5 +1,24 @@
 
 export default class Response
 {
-	
+	/**
+	 * Construct a response (server-side)
+	 * 
+	 * @param 0.body     <mixed>
+	 * @param 0.?status  <number>
+	 * @param 0.?headers {}
+	 */
+	constructor( { body, status=200, headers={}, }, )
+	{
+		this.status= status;
+		this.headers= new Headers( headers, );
+		
+		if( typeof body === 'string' )
+		{
+			this.body= new TextEncoder().encode( body, );
+			this.headers.set( 'Content-Length', this.body.length, )
+		}
+		else
+			this.body= body;
+	}
 }
