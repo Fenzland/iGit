27cf5bd58776312e7f0dcde5f8e2bb6e4d09f319
Fenz <uukoo@163.com>
2019-04-14 01:07:17 +0800

    Router add controller path param, and pass into route

--- dragonfly/routing/Router.js
+++ dragonfly/routing/Router.js
@@ -1,3 +1,4 @@
+import { path, } from '../packages.js';
 import Route from './Route.js';
 
 export default class Router
@@ -6,10 +7,18 @@ export default class Router
 	 * Consturct a router
 	 * 
 	 * @param 0.routes          {}[]
+	 * @param 0.?controllerPath (string)
 	 */
-	constructor( { routes, }, )
+	constructor( { routes, controllerPath=defaultWebRoot(), }, )
 	{
-		this.routes= routes.map( route=> new Route( route, ), );
+		this.controllerPath=controllerPath;
+		
+		this.routes= routes.map( route=> {
+			if( typeof route.controller === 'string' )
+				route.controller= controllerPath + route.controller;
+			
+			return new Route( route, );
+		}, );
 	}
 	
 	/**
@@ -23,4 +32,16 @@ export default class Router
 	{
 		return this.routes[0];
 	}
+}
+
+/**
+ * Generate the default web root
+ * 
+ * @return (string)
+ */
+function defaultWebRoot()
+{
+	const file= path.traceBack( 2, );
+	
+	return `${path.dirname( file, )}/controllers`;
 }
