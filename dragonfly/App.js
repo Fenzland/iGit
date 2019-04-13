b1070271822f1d42c120f7d728f49e638d576c94
Fenz <uukoo@163.com>
2019-04-14 01:44:26 +0800

    if the request path matches a file in the webRoot, dragonfly respond the file

--- dragonfly/App.js
+++ dragonfly/App.js
@@ -1,6 +1,7 @@
-import { httpServer, path, } from './modules.deno.js';
+import { httpServer, path, file_exists, } from './modules.deno.js';
 import Request from './http/Request.js';
 import Response from './http/Response.js';
+import { ext2mime, } from './http/mime.js';
 
 /**
  * main Application class of Dragonfly
@@ -32,12 +33,35 @@ export default class App
 		{
 			const request= new Request( denoRequest, );
 			
+			if( await this.hasFile( request.path, ) )
+			{
+				const path= `${this.webRoot}${request.path}`;
+				
+				denoRequest.respond( await makeFileResponse( path, ), );
+				
+				continue;
+			}
+			
 			const route= this.router.dispatch( request, );
 			
 			const response= await route.run( request, );
 			
 			denoRequest.respond( response, );
 		}
+	}
+	
+	/**
+	 * Check whether a file exists in web root.
+	 * 
+	 * @param path (string)
+	 * 
+	 * @return ~(boolean)
+	 */
+	async hasFile( path, )
+	{
+		path= `${this.webRoot}${path}`;
+		
+		return file_exists( path, ) && await is_file( path, );
 	}
 }
 
@@ -51,4 +75,43 @@ function defaultWebRoot()
 	const file= path.traceBack( 2, );
 	
 	return path.dirname( file, ).replace( /^file:\/\//, '', );
+}
+
+/**
+ * Check whether a file is a file (not directory or symbol link).
+ * 
+ * @param path (string)
+ * 
+ * @return ~(boolean)
+ */
+async function is_file( path, )
+{
+	return (await Deno.stat( path, )).isFile();
+}
+
+/**
+ * Make file response
+ * 
+ * @access private
+ * 
+ * @param path (string)
+ * 
+ * @return ~{Response}
+ */
+async function makeFileResponse( path, )
+{
+	const ext= (x=> x? x[0]: '')( path.match( /\.\w+$/, ), );
+	const mime= ext2mime( ext, ) || 'text/plain';
+	
+	const file= Deno.open( path, );
+	const stat= Deno.stat( path, );
+	
+	return new Response( {
+		body: await file,
+		status: 200,
+		headers: {
+			'Content-Type': mime,
+			'Content-Length': (await stat).len,
+		},
+	}, );
 }
--- dragonfly/modules.deno.js
+++ dragonfly/modules.deno.js
@@ -1,2 +1,3 @@
 export * as httpServer from 'https://deno.land/x/std@v0.3.2/http/server.ts';
+export { exists as file_exists, } from 'https://deno.land/x/std@v0.3.2/fs/exists.ts';
 export * as path from 'https://oxo.fenzland.com/OsO/0.1/path.js';
