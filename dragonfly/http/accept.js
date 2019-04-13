f8144ac1921a57d805502be780ec77f4b08a49e3
Fenz <uukoo@163.com>
2019-04-13 21:58:03 +0800

    Accept add extension name parsing, as the first class accept

--- dragonfly/http/accept.js
+++ dragonfly/http/accept.js
@@ -1,3 +1,4 @@
+import { ext2mime, } from './mime.js';
 
 export class AcceptArray extends Array
 {
@@ -5,10 +6,20 @@ export class AcceptArray extends Array
 	 * Construct a accept array
 	 * 
 	 * @param acceptString  (string)  Request header Accept
+	 * @param ?path         (string)  Request path, for extension check
 	 */
-	constructor( acceptString, )
+	constructor( acceptString, path='/', )
 	{
 		super();
+		
+		const ext= (x=> x? x[0]: '')( path.match( /\.\w+$/, ), );
+		if( ext )
+		{
+			const mime= ext2mime( ext, );
+			
+			if( mime )
+				this.push( new AcceptItem( mime, ), );
+		}
 		
 		acceptString.split( ',', ).forEach( item=> {
 			let [ mime, quality=1, ]= item.split( ';q=', );
