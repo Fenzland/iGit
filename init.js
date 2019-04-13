acd39301312d1e6045e877f9c134095f15bb5cec
Fenz <uukoo@163.com>
2019-04-13 20:58:31 +0800

    add asycn helpers timeout(), nextFrame()

--- init.js
+++ init.js
@@ -1,2 +1,5 @@
 
 globalThis.z= ( x, ...a )=> (console.log( x, ...a, ), x);
+
+globalThis.timeout= ( time, value, )=> new Promise( resolve=> setTimeout( ()=> resolve( value, ), time, ), );
+globalThis.nextFrame= value=> new Promise( resolve=> requestAnimationFrame( ()=> resolve( value, ), ), );
