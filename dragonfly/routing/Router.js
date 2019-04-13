288070d22277205954e624b15b3d11face7956f4
Fenz <uukoo@163.com>
2019-04-14 01:01:50 +0800

    Router build with routes, dispatches a request and returns a route

--- dragonfly/routing/Router.js
+++ dragonfly/routing/Router.js
@@ -1,5 +1,26 @@
+import Route from './Route.js';
 
 export default class Router
 {
+	/**
+	 * Consturct a router
+	 * 
+	 * @param 0.routes          {}[]
+	 */
+	constructor( { routes, }, )
+	{
+		this.routes= routes.map( route=> new Route( route, ), );
+	}
 	
+	/**
+	 * Dispatch a request to a route
+	 * 
+	 * @param request {Request}
+	 * 
+	 * @return {Route}
+	 */
+	dispatch( request, )
+	{
+		return this.routes[0];
+	}
 }
