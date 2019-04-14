19547b6ef6659a46f17048d2f67634c0e4d247e6
Fenz <uukoo@163.com>
2019-04-14 10:02:30 +0800

    add class cli/Args to parse the CIL args

---
+++ cli/Args.js
@@ -0,0 +1,59 @@
+
+export default class Args
+{
+	/**
+	 * @param ...args (string)
+	 */
+	constructor( ...args )
+	{
+		this.args= args;
+		this.params= [];
+		this.options= {};
+		
+		for( const arg of args )
+		{
+			if( arg.startsWith( '--' ) )
+			{
+				const [ option, ...rest ]= arg.slice( 2, ).split( '=', );
+				
+				this.options[option]= rest.length? rest.join( '=', ): true;
+			}
+			else
+			if( arg.startsWith( '-', ) )
+			{
+				const options= arg.slice( 1, ).split( '', );
+				
+				for( const option of options )
+					this.options[option]= true;
+			}
+			else
+				this.params.push( arg, );
+		}
+	}
+	
+	/**
+	 * check whether the argument bag has the given option.
+	 * 
+	 * @param option (string)  option name, must without '-' or '--'
+	 */
+	hasOption( option, )
+	{
+		return this.options.hasOwnProperty( option, );
+	}
+	
+	/**
+	 * check whether the argument bag has the given option.
+	 * 
+	 * @param option (string)     option name, must without '-' or '--'
+	 * @param deflt  (string)     default value
+	 *               =>(string)   default value
+	 */
+	getOption( option, deflt=undefined, )
+	{
+		return (
+			this.hasOption( option, )? this.options[option]: 
+			deflt instanceof Function? deflt(): deflt
+		);
+	}
+	
+}
