a9dc48b7d8b5f695c51bf05b8afe7f39ec400dd6
Fenz <uukoo@163.com>
2019-04-14 11:28:44 +0800

    add controller index and add the route

---
+++ app/controllers/index.js
@@ -0,0 +1,5 @@
+
+export default async ( { request, Response, }, )=> {
+	
+	return Response.newHTML( '', );
+};
--- app/router.js
+++ app/router.js
@@ -2,5 +2,12 @@ import Router from '../dragonfly/routing/Router.js';
 
 export default new Router( {
 	controllerPath: import.meta.url.replace( /router\.js$/, 'controllers/', ),
-	routes: [],
+	routes: [
+		{
+			path: '/',
+			method: 'GET',
+			accept: 'text/html',
+			controller: 'index.js',
+		},
+	],
 }, );
