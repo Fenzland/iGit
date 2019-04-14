47245be65b45c5a5d383bbb7f8bd68791890aad5
Fenz <uukoo@163.com>
2019-04-14 11:04:43 +0800

    iGit listens HTTP with dragonfly

--- iGit.js
+++ iGit.js
@@ -1,1 +1,14 @@
 import './init.js';
+import Args from './cli/Args.js';
+import App from './dragonfly/App.js';
+
+(async args=> {
+	args= new Args( ...args );
+	
+	const host= args.getOption( 'host', '0.0.0.0:8888', );
+	
+	const app= new App();
+	
+	app.listenHTTP( host, );
+	
+})();
