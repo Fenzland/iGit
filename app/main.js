15da859be7576980b4b961cfc096bb856c525d14
Fenz <uukoo@163.com>
2019-04-14 12:16:10 +0800

    use Vue at front end

--- app/main.js
+++ app/main.js
@@ -1,1 +1,16 @@
 import '../init.js';
+import { Vue, } from './modules.web.js';
+
+Vue.prototype.z= z;
+
+const app= new Vue( {
+	el: 'body>main',
+	
+	template: /*HTML*/`
+		<main>
+			<header>
+				<h1> iGit </h1>
+			</header>
+		</main>
+	`,
+}, );
---
+++ app/modules.web.js
@@ -0,0 +1,1 @@
+export { default as Vue } from 'https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.esm.browser.js';
