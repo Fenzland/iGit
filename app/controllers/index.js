d872015bdbdf5c50b8fd985a402e2c30f436f41c
Fenz <uukoo@163.com>
2019-04-14 12:02:43 +0800

    add base files for browser side

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,6 @@
+import document from '../layout.js';
 
 export default async ( { request, Response, }, )=> {
 	
-	return Response.newHTML( '', );
+	return Response.newHTML( document, );
 };
---
+++ app/layout.js
@@ -0,0 +1,16 @@
+
+export default /*HTML*/`
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>iGit</title>
+		<link rel="stylesheet" type="text/css" href="/app/layout.css" />
+		<link rel="stylesheet" type="text/css" href="/app/style.css" />
+	</head>
+	<body>
+		<main id="main"></main>
+		<script type="module" src="/app/main.js"></script>
+	</body>
+</html>
+`;
