7039b8d91e720571eae1e53239e007d774bf1ca0
Fenz <uukooo@126.com>
2019-05-16 00:32:38 +0800

    add git version checker. we need git version >= 2.18

--- git/Git.js
+++ git/Git.js
@@ -19,5 +19,20 @@ export default class Git
 			return this;
 		})();
 	}
+	
+	/**
+	 * Check git version.
+	 * 
+	 * @return ~
+	 * 
+	 * @throw {Error}
+	 */
+	static async checkVersion()
+	{
+		const v= (await GitCli.run( '--version', )).replace( 'git version ', '', ).split( '.', ).map( x=> +x, );
+		
+		if(!( v[0] >= 2 && v[1] >= 18 ))
+			throw new Error( 'iGit need git version >= 2.18', );
+	}
 }
 
