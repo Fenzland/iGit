6efd73c5bb3cec833c9bb046070b41e1dfc32b45
Fenz <uukooo@126.com>
2019-05-24 23:55:07 +0800

    constructor of Git, get git-dir, work-tree and is-bare by git command

--- git/Git.js
+++ git/Git.js
@@ -3,36 +3,62 @@ import GitCli from './cli/GitCli.js';
 export default class Git
 {
 	/**
+	 * Git dir
+	 * 
+	 * @type (string)
+	 */
+	#gitDir;
+	
+	/**
+	 * Work tree
+	 * 
+	 * @type ?(string)
+	 */
+	#workTree;
+	
+	/**
+	 * Is bare
+	 * 
+	 * @type (boolean)
+	 */
+	#isBare;
+	
+	/**
 	 * Construct a git repository
 	 * 
-	 * @param gitDir    (string)
-	 * @param ?workTree (string)  empty for bare repository
+	 * @param gitDir   ?(string)
+	 * @param workTree ?(string)
 	 * 
 	 * @return ~{Git}
 	 */
-	constructor( gitDir, workTree=undefined, )
+	constructor( gitDir=undefined, workTree=undefined, )
 	{
-		this.cli= new GitCli( gitDir, workTree, );
-		
 		return (async ()=> {
 			
+			if( gitDir )
+				this.#gitDir= gitDir;
+			else
+				this.#gitDir= (await GitCli.run( 'rev-parse', '--absolute-git-dir', )).trim();
+			
+			this.#isBare= 'true' === (await GitCli.run( 'rev-parse', '--is-bare-repository', )).trim();
+			
+			if( workTree )
+				this.#workTree= workTree;
+			else
+			if(!( this.#isBare ))
+				this.#workTree= (await GitCli.run( 'rev-parse', '--show-toplevel', )).trim();
+			else
+				this.#workTree= null;
+			
+			this.cli= new GitCli( this.#gitDir, this.#workTree, );
+			
+			
+			await this.cli.getHEAD();
+			
 			return this;
-		})();
-	}
-	
-	/**
-	 * Check git version.
-	 * 
-	 * @return ~
-	 * 
-	 * @throw {Error}
-	 */
-	static async checkVersion()
-	{
-		const v= (await GitCli.run( '--version', )).replace( 'git version ', '', ).split( '.', ).map( x=> +x, );
-		
-		if(!( v[0] >= 2 && v[1] >= 18 ))
-			throw new Error( 'iGit need git version >= 2.18', );
+		})().catch( ()=> {
+			throw new Error( 'iGit must run in a valid git repository.', );
+		}, );
 	}
 }
 
