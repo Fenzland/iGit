9058d3fdbc10aaaca44b6748f7bb3acb5f2bb3d2
Fenz <uukooo@126.com>
2019-06-02 13:42:36 +0800

    use Vuejs to build UI

--- app/main.js
+++ app/main.js
@@ -1,1 +1,20 @@
 import '../init.js';
+import { Vue, } from './modules.web.js';
+
+Vue.prototype.z= z;
+
+const app= new Vue( {
+	el: 'body>main',
+	
+	template: /*HTML*/`
+		<main>
+			<header>
+				<h1> <img class="logo" src="/logo.svg">iGit </h1>
+				<span class="version"> v0.2.0 </span>
+			</header>
+		</main>
+	`,
+	
+	data: {
+	},
+}, );
---
+++ app/modules.web.js
@@ -0,0 +1,2 @@
+export { default as Vue } from 'https://cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.esm.browser.js';
+export { default as Vuex } from 'https://unpkg.com/vuex@3.1.1/dist/vuex.esm.browser.js';
