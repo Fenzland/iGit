184dbf165349fe0e7c68d998b2cabda5b61522a0
Fenz <uukooo@126.com>
2019-06-02 19:15:37 +0800

    improve main component

--- app/main.js
+++ app/main.js
@@ -25,7 +25,7 @@ const app= new Vue( {
 	},
 	
 	template: /*HTML*/`
-		<main>
+		<main :style="{ '--second-row-size':(secondRowOpened? secondRowSize: 0), '--second-row-size-offset':(secondRowOpened? secondRowSizeOffset: 0), }">
 			<header>
 				<h1> <img class="logo" src="/logo.svg">iGit </h1>
 				<span class="version"> v0.2.0 </span>
@@ -38,5 +38,141 @@ const app= new Vue( {
 	`,
 	
 	data: {
+		focusedGridName: null,
+		secondRowOpened: false,
+		secondRowSize: 8,
+		secondRowSizeOffset: 0,
 	},
+	
+	computed: {
+		focusedGrid(){
+			return this.$refs[this.focusedGridName];
+		},
+	},
+	
+	methods: {
+		async openSecondRow(){
+			if(!( this.secondRowOpened ))
+			{
+				this.secondRowOpened= true;
+				
+				await timeout();
+				
+				this.resize();
+			}
+		},
+		
+		async closeSecondRow(){
+			if( this.secondRowOpened )
+			{
+				this.secondRowOpened= false;
+				
+				await timeout();
+				
+				this.resize();
+			}
+		},
+		
+		focusToGrid( grid, ){
+			this.focusedGridName= grid;
+		},
+		
+		keydown( key, ){
+			
+			// z( key, 'keydown', );
+			
+			if( this.focusedGridName )
+				return this.focusedGrid.keydown( key, );
+			
+			switch( key.key )
+			{
+				case 'h':
+					return this.focusToGrid( 'refs', ), true;
+				break;
+				
+				case 't':
+					return this.focusToGrid( 'graph', ), true;
+				break;
+				
+				case 'n':
+					return this.focusToGrid( 'changed_files', ), true;
+				break;
+				
+				case 's':
+					return this.focusToGrid( 'content_diff', ), true;
+				break;
+			}
+		},
+		
+		keyup( key, ){
+			
+			// z( key, 'keyup' );
+			
+			if( this.focusedGridName )
+				return this.$refs[this.focusedGridName].keyup( key, );
+			
+			switch( key.key )
+			{
+			}
+		},
+		
+		blurGrid( grid, )
+		{
+			if( this.focusedGridName===grid )
+				this.focusedGridName= null;
+		},
+		
+		onWindowBlur()
+		{
+			if( this.focusedGridName && this.focusedGrid.onWindowBlur )
+				this.focusedGrid.onWindowBlur();
+		},
+		
+		resize(){
+			Object.values( this.$refs, ).forEach( ref=> {
+				if( ref.resize && ref.resize instanceof Function )
+					ref.resize();
+			}, );
+		},
+	},
+	mounted(){
+		this.$eve.listen( 'focus-to-file', this.openSecondRow, )
+		this.$eve.listen( 'blur-file', this.closeSecondRow, )
+		this.$eve.listen( 'grid-focus', this.focusToGrid, )
+	},
+}, );
+
+window.addEventListener( 'keydown', handleKey, );
+window.addEventListener( 'keyup', handleKey, );
+
+function handleKey( event, )
+{
+	const modifiers= [];
+	
+	if( event.metaKey )
+		modifiers.push( 'Meta', );
+	
+	if( event.ctrlKey )
+		modifiers.push( 'Ctrl', );
+	
+	if( event.altKey )
+		modifiers.push( 'Alt', );
+	
+	if( event.shiftKey )
+		modifiers.push( 'Shift', );
+	
+	const effective= app[event.type]( {
+		key: event.key,
+		modifiers,
+	}, );
+	
+	if( effective )
+		event.preventDefault();
+}
+
+window.addEventListener( 'blur', event=> {
+	app.onWindowBlur();
+}, );
+window.addEventListener( 'resize', event=> {
+	app.resize();
 }, );
