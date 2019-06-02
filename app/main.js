7879918b99184ca3220a267877738be7fcf31915
Fenz <uukooo@126.com>
2019-06-02 19:13:47 +0800

    use each grid into main component

--- app/main.js
+++ app/main.js
@@ -1,6 +1,10 @@
 import '../init.js';
 import { Vue, } from './modules.web.js';
 import Eve from './Eve.js';
+import gridRefs from './components/grid-refs.js';
+import gridGraph from './components/grid-graph.js';
+import gridChangedFiles from './components/grid-changed-files.js';
+import gridContentDiff from './components/grid-content-diff.js';
 
 Vue.prototype.z= z;
 Vue.use( Eve, );
@@ -13,12 +17,23 @@ Vue.prototype.formatTime= time=> new Date( time, ).toLocaleString(
 const app= new Vue( {
 	el: 'body>main',
 	
+	components: {
+		gridRefs,
+		gridGraph,
+		gridChangedFiles,
+		gridContentDiff,
+	},
+	
 	template: /*HTML*/`
 		<main>
 			<header>
 				<h1> <img class="logo" src="/logo.svg">iGit </h1>
 				<span class="version"> v0.2.0 </span>
 			</header>
+			<grid-refs          ref="refs"          :focused="focusedGridName==='refs'"          v-on:blur="blurGrid( 'refs', )"          v-on:click.native.capture="focusedGridName= 'refs'"          ></grid-refs>
+			<grid-graph         ref="graph"         :focused="focusedGridName==='graph'"         v-on:blur="blurGrid( 'graph', )"         v-on:click.native.capture="focusedGridName= 'graph'"         ></grid-graph>
+			<grid-changed-files ref="changed_files" :focused="focusedGridName==='changed_files'" v-on:blur="blurGrid( 'changed_files', )" v-on:click.native.capture="focusedGridName= 'changed_files'" ></grid-changed-files>
+			<grid-content-diff  ref="content_diff"  :focused="focusedGridName==='content_diff'"  v-on:blur="blurGrid( 'content_diff', )"  v-on:click.native.capture="focusedGridName= 'content_diff'"  ></grid-content-diff>
 		</main>
 	`,
 	
