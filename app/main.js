38773446eb55c5d6b71ea56bdf2329998e8bd234
Fenz <uukooo@126.com>
2019-06-02 19:12:43 +0800

    add time format support

--- app/main.js
+++ app/main.js
@@ -5,6 +5,11 @@ import Eve from './Eve.js';
 Vue.prototype.z= z;
 Vue.use( Eve, );
 
+Vue.prototype.formatTime= time=> new Date( time, ).toLocaleString(
+	navigator.language,
+	{ year: 'numeric', month: '2-digit', day: '2-digit', weekday: 'long', hour: '2-digit', hour12: false, minute: '2-digit', },
+)
+
 const app= new Vue( {
 	el: 'body>main',
 	
