dc7c15c5a9a61ea12d0c900fc1ce41c6586c9dd7
Fenzland <Fenzland@github.com>
2019-08-18 19:43:13 +0800

    change not-found from route to controller

--- dragonfly/routing/not-found.js
+++ dragonfly/routing/not-found.js
@@ -5,12 +5,10 @@ const html= /*HTML*/`
 <h1>404 Page Not Found</h1>
 `;
 
-export default new Route( {
-	controller: ( { request, }, )=> {
-		
-		if( request.accept.match( 'application/json', ) > 1 )
-			return Response.newJSON( { status:404, message:'Not Found', }, { status: 404, }, );
-		else
-			return Response.newHTML( html, { status: 404, }, );
-	},
-}, )
+export default ( { request, }, )=> {
+	
+	if( request.accept.match( 'application/json', ) > 1 )
+		return Response.newJSON( { status:404, message:'Not Found', }, { status: 404, }, );
+	else
+		return Response.newHTML( html, { status: 404, }, );
+};
